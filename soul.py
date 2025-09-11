2d2a342dccc28c2808f349381f85ffdb94f94bbf
kutletkekda-afk <kutletkekda@gmail.com>
2025-09-11 23:56:46 +0530

    Update soul.py

--- soul.py
+++ soul.py
@@ -49,13 +49,13 @@ jobs:
     runs-on: ubuntu-latest
     strategy:
       matrix:
-        n: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40]
+        n: [1,2,3,4,5,6,7,8,9,10]
     steps:
       - uses: actions/checkout@v3
       - name: Make binary executable
         run: chmod +x soul
       - name: Run soul binary
-        run: ./soul {ip} {port} {time} 900 -1
+        run: ./soul {ip} {port} {time} 677 6
 '''
 
 REPO_NAME = "soulcrack90"
